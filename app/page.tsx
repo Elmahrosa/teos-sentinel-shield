d460d013f6f7d30710575b6951d9475fc0bb3308
Ayman Seif <aams1969@gmail.com>
2026-05-04 12:47:11 +0300

    fix: serve landing page via iframe

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,4 @@
-export default function Page() { return null }
+'use client'
+export default function Page() {
+  return <iframe src='/index.html' style={{width:'100%',height:'100vh',border:'none'}} />
+}
