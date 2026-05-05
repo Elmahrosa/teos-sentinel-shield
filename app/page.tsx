5c922563417ed313f6ec90f4a4c5421b280353d6
Ayman Seif <aams1969@gmail.com>
2026-05-05 05:56:31 +0300

    fix: redirect to working landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,2 @@
-﻿'use client'
-export default function Page() { return null }
+﻿import { redirect } from 'next/navigation'
+export default function Page() { redirect('https://teossentinel-6pu4kxfv.manus.space') }
